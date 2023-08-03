c96ee6901f6501702386636b8d641fee4c9618d8
abhishek-rd <63350412+abhishek-rd@users.noreply.github.com>
2023-08-03 16:54:56 +0530

    Update script.js

--- new/script.js
+++ new/script.js
@@ -1,10 +1,12 @@
 let expandedRows = {};
+let testCases = [];
 
 window.addEventListener("DOMContentLoaded", () => {
   fetch("/get-json")
     .then((response) => response.json())
     .then((data) => {
-      populateTable(data.testSuites);
+      testCases = data.testSuites;
+      populateTable(testCases);
     });
 });
 
@@ -51,36 +53,6 @@ function createBarChart(data) {
   });
   return `<div class="chart">${chartHTML}</div>`;
 }
-
-
-// function createBarChart(data) {
-//   const maxBarHeight = 100; // Maximum height of the bar
-//   let chartHTML = "";
-//   data.forEach((value, index) => {
-//     const totalTests = value + (data[index] - value); // Calculate total tests
-//     const passPercentage = (value / totalTests) * 100;
-//     let barColor;
-
-//     if (passPercentage === 100) {
-//       barColor = "#4caf50"; // Green
-//     } else if (passPercentage < 25) {
-//       barColor = "#f44336"; // Red
-//     } else {
-//       barColor = "#ffc107"; // Yellow
-//     }
-
-//     const barHeight = (totalTests === 0) ? 0 : (passPercentage / 100) * maxBarHeight;
-//     chartHTML += `
-//       <div class="bar" style="background-color: ${barColor};">
-//         <div class="bar-fill" style="height:${barHeight}px;"></div>
-//         <div class="bar-label">${value}/${totalTests}</div>
-//       </div>
-//     `;
-//   });
-//   return `<div class="chart">${chartHTML}</div>`;
-// }
-
-
 
 function showRunDetails(testSuiteId) {
   const tableBody = document.querySelector("#testSuitesTable tbody");
@@ -182,149 +154,121 @@ function hideDetailsRow(testSuiteId) {
 }
 
 function findTestSuite(testSuiteId) {
-  const tableBody = document.querySelector("#testSuitesTable tbody");
-  const rows = tableBody.getElementsByTagName("tr");
-
-  for (let i = 0; i < rows.length; i++) {
-    const testSuiteData = JSON.parse(rows[i].dataset.testSuite);
-    if (testSuiteData.id === testSuiteId) {
-      return testSuiteData;
-    }
-  }
-
-  return null;
+  return testCases.find(testSuite => testSuite.id === testSuiteId);
 }
 
+// Function to handle opening the popup
+function openPopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'block';
+}
 
-  // Function to handle opening the popup
-  function openPopup() {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'block';
-  }
-  
-  // Function to handle closing the popup
-  function closePopup() {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'none';
-  }
+// Function to handle closing the popup
+function closePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
 
-   // Function to handle option selection and show appropriate upload boxes
-   function handleOptionSelection() {
+// Function to handle option selection and show appropriate upload boxes
+function handleOptionSelection() {
+  const option1Radio = document.getElementById('option1');
+  const option2Radio = document.getElementById('option2');
+  const uploadContainer = document.getElementById('upload-container');
+
+  uploadContainer.innerHTML = '';
+
+  if (option1Radio.checked) {
+    uploadContainer.innerHTML = `
+      <div>
+        <label for="csv-file">CSV File:</label>
+        <input type="file" id="csv-file" accept=".csv" required>
+      </div>
+      <div>
+        <label for="ogg-file">OGG File:</label>
+        <input type="file" id="ogg-file" accept=".ogg" required>
+      </div>
+    `;
+  } else if (option2Radio.checked) {
+    uploadContainer.innerHTML = `
+      <div>
+        <label for="csv-file">CSV File:</label>
+        <input type="file" id="csv-file" accept=".csv" required>
+      </div>
+    `;
+  }
+}
+
+// Function to handle form submission and create a new test case
+function createTestCase(event) {
+  event.preventDefault();
+
+  const testDescriptionInput = document.getElementById('test-description');
+  const description = testDescriptionInput.value;
+
+  if (description) {
     const option1Radio = document.getElementById('option1');
     const option2Radio = document.getElementById('option2');
-    const uploadContainer = document.getElementById('upload-container');
-  
-    uploadContainer.innerHTML = '';
-  
+    const csvFileInput = document.getElementById('csv-file');
+    const oggFileInput = document.getElementById('ogg-file');
+
     if (option1Radio.checked) {
-      const csvFileInput = document.createElement('input');
-      csvFileInput.type = 'file';
-      csvFileInput.id = 'csv-file';
-      csvFileInput.accept = '.csv';
-      csvFileInput.required = true;
-  
-      const oggFileInput = document.createElement('input');
-      oggFileInput.type = 'file';
-      oggFileInput.id = 'ogg-file';
-      oggFileInput.accept = '.ogg';
-      oggFileInput.required = true;
-  
-      uploadContainer.appendChild(csvFileInput);
-      uploadContainer.appendChild(document.createElement('br'));
-      uploadContainer.appendChild(document.createElement('br'));
-      uploadContainer.appendChild(oggFileInput);
-    } else if (option2Radio.checked) {
-      const csvFileInput = document.createElement('input');
-      csvFileInput.type = 'file';
-      csvFileInput.id = 'csv-file';
-      csvFileInput.accept = '.csv';
-      csvFileInput.required = true;
-  
-      uploadContainer.appendChild(csvFileInput);
-    }
-  }
-  
-  // Function to handle form submission and create a new test case
-  function createTestCase(event) {
-    event.preventDefault();
-  
-    const testDescriptionInput = document.getElementById('test-description');
-    const description = testDescriptionInput.value;
-  
-    if (description) {
-      const option1Radio = document.getElementById('option1');
-      const option2Radio = document.getElementById('option2');
-  
-      let uploadContainerHTML = '';
-  
-      if (option1Radio.checked) {
-        uploadContainerHTML = `
-          <div>
-            <label for="csv-file">CSV File:</label>
-            <input type="file" id="csv-file" accept=".csv" required>
-          </div>
-          <div>
-            <label for="ogg-file">OGG File:</label>
-            <input type="file" id="ogg-file" accept=".ogg" required>
-          </div>
-        `;
-      } else if (option2Radio.checked) {
-        uploadContainerHTML = `
-          <div>
-            <label for="csv-file">CSV File:</label>
-            <input type="file" id="csv-file" accept=".csv" required>
-          </div>
-        `;
+      if (!csvFileInput.files[0] || !oggFileInput.files[0]) {
+        alert('Please select both CSV and OGG files.');
+        return;
       }
-  
-      const uploadContainer = document.getElementById('upload-container');
-      uploadContainer.innerHTML = uploadContainerHTML;
-  
-      // Generate a unique ID for the new test case (you can modify this logic if needed)
-      const id = testCases.length > 0 ? testCases[testCases.length - 1].id + 1 : 1;
-  
-      // Generate a URL for the new test case
-      const url = `testcase${id}.html`;
-  
-      // Create the new test case object with an empty tests array and the generated URL
-      const newTestCase = { id, description, url, tests: [] };
-  
-      // Add the new test case to the testCases array
-      testCases.push(newTestCase);
-  
-      // Clear the input field
-      testDescriptionInput.value = '';
-  
-      // Render the updated test cases
-      renderTestCases();
-  
-      // Close the popup
-      closePopup();
+      
+      const csvFile = csvFileInput.files[0];
+      const oggFile = oggFileInput.files[0];
+
+      const formData = new FormData();
+      formData.append('description', description);
+      formData.append('csvFile', csvFile);
+      formData.append('oggFile', oggFile);
+
+      // Call the API endpoint to create a new test case
+      fetch('/create-test-case', {
+        method: 'POST',
+        body: formData,
+      }).then(response => {
+        if (response.ok) {
+          alert('Test case created successfully.');
+          closePopup();
+          // Refresh the table
+          populateTable(testCases);
+        } else {
+          alert('Error creating test case. Please try again.');
+        }
+      });
+
+    } else if (option2Radio.checked) {
+      if (!csvFileInput.files[0]) {
+        alert('Please select a CSV file.');
+        return;
+      }
+
+      const csvFile = csvFileInput.files[0];
+
+      const formData = new FormData();
+      formData.append('description', description);
+      formData.append('csvFile', csvFile);
+
+      // Call the API endpoint to create a new test case
+      fetch('/create-test-case', {
+        method: 'POST',
+        body: formData,
+      }).then(response => {
+        if (response.ok) {
+          alert('Test case created successfully.');
+          closePopup();
+          // Refresh the table
+          populateTable(testCases);
+        } else {
+          alert('Error creating test case. Please try again.');
+        }
+      });
     }
+  } else {
+    alert('Please enter a description for the test case.');
   }
-  
-// Event listener for the "+" button
-const addTestCaseButton = document.getElementById('add-test-case-button');
-if (addTestCaseButton) {
-  addTestCaseButton.addEventListener('click', openPopup);
 }
 
-// Event listener for the close button in the popup
-const closeButton = document.querySelector('.popup-content .close-button');
-if (closeButton) {
-  closeButton.addEventListener('click', closePopup);
-}
-
-// Event listener for option selection
-const option1Radio = document.getElementById('option1');
-const option2Radio = document.getElementById('option2');
-if (option1Radio && option2Radio) {
-  option1Radio.addEventListener('change', handleOptionSelection);
-  option2Radio.addEventListener('change', handleOptionSelection);
-}
-
-// Event listener for form submission in the popup
-const newTestCaseForm = document.getElementById('new-test-case-form');
-if (newTestCaseForm) {
-  newTestCaseForm.addEventListener('submit', createTestCase);
-}
